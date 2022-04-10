7bb64b1f689869b5ab27e8f3d1e4027e9e8400ff
Simen A. W. Olsen <so@bjerk.io>
2022-04-10 19:04:07 +0200

    comment out ledger account

--- src/calls/ledger-account/ledger-account.ts
+++ src/calls/ledger-account/ledger-account.ts
@@ -1,17 +1,17 @@
-import * as rt from 'runtypes';
-import { withRuntype } from '../../utils';
-import { TripletexBase } from '../base';
-import { ledgerAccountListRequest, listResponseAccountRt } from './models/list';
+// import * as rt from 'runtypes';
+// import { withRuntype } from '../../utils';
+// import { TripletexBase } from '../base';
+// import { ledgerAccountListRequest, listResponseAccountRt } from './models/list';
 
-export class TripletexLedgerAccount extends TripletexBase {
-  list() {
-    const call = this.authenticatedCall()
-      .args<rt.Static<typeof ledgerAccountListRequest>>()
-      .path('/ledger/account')
-      .method('get')
-      .parseJson(withRuntype(listResponseAccountRt))
-      .build();
+// export class TripletexLedgerAccount extends TripletexBase {
+//   list() {
+//     const call = this.authenticatedCall()
+//       .args<rt.Static<typeof ledgerAccountListRequest>>()
+//       .path('/ledger/account')
+//       .method('get')
+//       .parseJson(withRuntype(listResponseAccountRt))
+//       .build();
 
-    return this.performRequest(() => call({}));
-  }
-}
+//     return this.performRequest(() => call(}));
+//   }
+// }
