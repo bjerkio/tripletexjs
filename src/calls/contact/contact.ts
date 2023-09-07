3801feda0a99d199873780056a63dac2cc4b147a
Brage Sekse Aarset <brage@bjerk.io>
2023-09-07 15:12:08 +0300

    feat: add contacts query

---
+++ src/calls/contact/contact.ts
@@ -0,0 +1,77 @@
+import { DefaultTripletexInputs } from "../../types";
+import { serializeQuery, withRuntype } from "../../utils";
+import { TripletexBase } from "../base";
+import { listEmployeesResponseRt } from "../employee/employee";
+
+export interface ListContactsInput extends DefaultTripletexInputs {
+  /**
+   * List of IDs
+   */
+  id?: string[];
+
+  /**
+   * Containing
+   */
+  firstName?: string;
+
+  /**
+   * Containing
+   */
+  lastName?: string;
+
+  /**
+   * Containing
+   */
+  email?: string;
+
+  /**
+   * List of IDs
+   */
+  customerId?: string;
+
+  /**
+   * List of IDs
+   */
+  departmentId?: string;
+
+  /**
+   * From index
+   */
+  includeContacts?: boolean;
+
+  /**
+   * List of IDs
+   */
+  deploymentId?: string[];
+ 
+  /**
+   * Number of elements to return
+   */
+  count: number;
+
+  /**
+   * Sorting pattern
+   */
+  sorting: string;
+
+  /**
+   * Fields filter pattern
+   */
+  fields: string;
+}
+
+export class TripletexContact extends TripletexBase {
+  list(input?: ListContactsInput) {
+    const call = this.authenticatedCall() //
+      .args<{
+        input?: ListContactsInput;
+      }>()
+      .path('/v2/employee')
+      .query(args => (args.input ? serializeQuery(args.input) : {}))
+      .method('get')
+      .parseJson(withRuntype(listEmployeesResponseRt))
+      .build();
+
+    return this.performRequest(sessionToken => call({ input, sessionToken }));
+  }
+}
---
+++ src/calls/contact/models/contact.ts
@@ -0,0 +1,32 @@
+import * as rt from 'runtypes';
+import { multipleValuesEnvelope } from '../../../utils';
+
+const contactRt = rt.Record({
+  id: rt.Number,
+  version: rt.Number.nullable().optional(),
+  url: rt.String,
+  firstName: rt.String,
+  lastName: rt.String,
+  displayName: rt.String.nullable().optional(),
+  email: rt.String.nullable().optional(),
+  phoneNumberMobileCountry: rt
+  .Record({
+    id: rt.Number,
+  })
+  .nullable()
+  .optional(),
+phoneNumberMobile: rt.String.nullable().optional(),
+phoneNumberWork: rt.String.nullable().optional(),
+customer: rt.Record({
+  id: rt.Number,
+}),
+department: rt.Record({
+  id: rt.Number,
+}),
+  isInactive: rt.Boolean.nullable().optional(),
+})
+
+export type Contact = rt.Static<typeof contactRt>;
+
+
+export const listContactsResponseRt = multipleValuesEnvelope(contactRt);
