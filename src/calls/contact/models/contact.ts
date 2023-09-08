8c6d6d3de4943aa2db36c1c1c68aa42195ad8a05
Brage Sekse Aarset <brage.aarset@gmail.com>
2023-09-08 10:14:18 +0300

    feat: add contacts query (#108)
    * feat: add contacts query
    
    * feat: add test

---
+++ src/calls/contact/models/contact.ts
@@ -0,0 +1,34 @@
+import * as rt from 'runtypes';
+import { multipleValuesEnvelope } from '../../../utils';
+
+const contactRt = rt.Record({
+  id: rt.Number,
+  version: rt.Number.nullable().optional(),
+  url: rt.String,
+  firstName: rt.String,
+  lastName: rt.String,
+  displayName: rt.String.nullable().optional(),
+  email: rt.String.nullable().optional(),
+  phoneNumberMobileCountry: rt
+  .Record({
+    id: rt.Number,
+  })
+  .nullable()
+  .optional(),
+  phoneNumberMobile: rt.String.nullable().optional(),
+  phoneNumberWork: rt.String.nullable().optional(),
+  customer: rt.Record({
+    id: rt.Number,
+    url: rt.String,
+  }),
+  department: rt.Record({
+    id: rt.Number,
+    url: rt.String,
+  }),
+  isInactive: rt.Boolean.nullable().optional(),
+})
+
+export type Contact = rt.Static<typeof contactRt>;
+
+
+export const listContactsResponseRt = multipleValuesEnvelope(contactRt);
